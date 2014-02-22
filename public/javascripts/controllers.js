1d6e384936a63210ca325300a7053de7434ad7cb
Tyler <twwaneka@gmail.com>
2014-02-22 15:29:29 -0800

    controller for basic skeleton
    - Create a new party
    - load a newly created party page
    - join a party as a viewer (currently no authorization either way)

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,20 +1,68 @@
-// Managing the parties list
-function PartiesCtrl($scope) {
-  $scope.parties = [];
-}
-// Voting / viewing poll results
-// function PollItemCtrl($scope, $routeParams) {
-//   $scope.poll = {};
-//   $scope.vote = function() {};
-// }
-// Creating a new poll
-// function PollNewCtrl($scope) {
-//   $scope.poll = {
-//     question: '',
-//     choices: [{ text: '' }, { text: '' }, { text: '' }]
-//   };
-//   $scope.addChoice = function() {
-//     $scope.poll.choices.push({ text: '' });
-//   };
-//   $scope.createPoll = function() {};
-// }
+'use strict'
+
+var queuedControllers = angular.module('queuedControllers', [])
+
+queuedControllers.controller('WelcomeCtrl', ['$scope', function($scope) {
+  // debugger
+  console.log('welcome!')
+}])
+
+queuedControllers.controller('JoinCtrl', ['$scope', function($scope) {
+  // debugger
+  console.log('join')
+}])
+
+queuedControllers.controller('PartyCreateCtrl', ['$scope', '$http', '$location',
+  function($scope, $http, $location) {
+    // $scope.form = {}
+    $scope.create = function() {
+      $http.post('/api/new_party').
+        success(function(data) {
+          console.log('success!!!')
+          console.log('data: ' + data.party_url)
+          $location.url('/party/' + data.party_url)
+        })
+    }
+    // some logic to create a new party page at the url: 'party/:url'
+    // post it to the database
+}])
+
+queuedControllers.controller('PartyCtrl', ['$scope', '$http', '$routeParams',
+  function($scope, $http, $routeParams) {
+    $http.get('/api/party/' + $routeParams.url).
+      success(function(data) {
+        $scope.party = data.party
+        console.log('party: ' + data.party.name)
+      })
+  // so, this should load the page that was created above ^^
+  // give that page any info it may need. load the search, queue/vote, and player stuff
+}])
+
+queuedControllers.controller('PartyJoinCtrl', ['$scope', '$http', '$location',
+  function($scope, $http, $location) {
+    $scope.submit = function() {
+      if ($scope.name) {
+        $http.get('/api/join_party/' + $scope.name).
+          success(function(data) {
+            debugger
+            console.log('Party ' + data.party.name + ' exists!')
+            $location.url('/party/' + data.party.party_url)
+          }).error(function(data, status) {
+            console.log('Error: ' + data)
+            debugger
+          })
+      }
+      // $http.get('/api/join_party/' + )
+    }
+}])
+
+queuedControllers.controller('PartiesCtrl', ['$scope', '$http',
+  function($scope, $http) {
+}])
+
+
+
+
+
+
+
